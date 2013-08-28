a8cb7af85d90b9dbc08caaac647fd43304f9dc97
ShuoX Liu <shuox.liu@gmail.com>
2013-08-28 07:19:30 -0700

    update mpu6050 support
    Signed-off-by: ShuoX Liu <shuox.liu@gmail.com>

--- mycar/work.c
+++ mycar/work.c
@@ -1,5 +1,6 @@
 #include "work.h"
 #include "mycar.h"
+#include "mpu6050.h"
 
 void led_high_worker(void)
 {
@@ -14,14 +15,24 @@ void led_low_worker(void)
 void log(void)
 {
     printf("os time:%ld\r\n", cpu_clock());
-    printf("accel xout: %d\r\n", mpu6050_get_data(0x3B));
-    printf("gyro xout: %d\r\n", mpu6050_get_data(0x43));
+    printf("%ld\n\r", mpu6050_get_data(0x3b));
+    printf("%ld\n\r", mpu6050_get_data(0x3d));
+    printf("%ld\n\r", mpu6050_get_data(0x3f));
+    /*
+    printf("%d,%d,%d,%d,%d,%d\r\n",
+            mpu6050_get_data(ACCEL_XOUT_H),
+            mpu6050_get_data(ACCEL_YOUT_H),
+            mpu6050_get_data(ACCEL_ZOUT_H),
+            mpu6050_get_data(GYRO_XOUT_H),
+            mpu6050_get_data(GYRO_YOUT_H),
+            mpu6050_get_data(GYRO_ZOUT_H));
+	    */
 }
 
 DEFINE_CODE_VAR(struct worker_entry, work_tbl[]) = {
     { led_low_worker,	100,	200, 1, true},
     { led_high_worker,	0,	200, 1, true},
-    { log,	0,	100, 1, true}
+    { log,		0,	100,   1, true}
 };
 
 DEFINE_CODE_VAR(u8,worker_num) = sizeof(work_tbl) / sizeof(struct worker_entry);
